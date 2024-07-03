beb6a93ab4ae933af4f4464168855130ace0db38
Manav Gosain <mu@Manavs-MacBook-Pro.local>
2024-07-04 00:54:20 +0530

    tried a few animations with nextJS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,207 @@
-import Image from "next/image";
+"use client";
+
+import React, { useEffect, useRef } from "react";
+import { motion, useAnimation, useInView, useScroll } from "framer-motion";
+
+const gridContainerVariants = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1, transition: { staggerChildren: 0.25 } },
+};
+
+const gridItemVariants = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
+
+const svgVariants = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+    fill: "rgba(255, 211, 77, 0)",
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    fill: "rgba(255, 211, 77, 1)",
+  },
+};
 
 export default function Home() {
+  const { scrollYProgress: completionProgress } = useScroll();
+
+  const containerRef = useRef(null);
+
+  const inView = useInView(containerRef);
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      mainControls.start("visible");
+    } else {
+      mainControls.start("invisible");
+    }
+  }, [inView]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex flex-col gap-10 overflow-x-hidden">
+      <motion.section
+        variants={gridContainerVariants}
+        initial="hidden"
+        animate="show"
+        transition={{ duration: 1, ease: "easeInOut" }}
+        className="grid grid-cols-3 p-10 gap-10"
+      >
+        {/* Fade in */}
+        <motion.div
+          variants={gridItemVariants}
+          className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
+        >
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: "easeInOut", delay: 0.5 }}
+            className="w-20 h-20 bg-stone-100 rounded-lg"
+          ></motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: "easeInOut", delay: 0.7 }}
+            className="w-20 h-20 bg-stone-100 rounded-full"
+          ></motion.div>
+        </motion.div>
+
+        {/* Shape shifting */}
+        <motion.div
+          variants={gridItemVariants}
+          className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
+        >
+          <motion.div
+            className="w-1/3 h-1/3 shadow-md bg-rose-400"
+            animate={{
+              scale: [1, 2, 1, 1],
+              rotate: [0, 90, 90, 0],
+              borderRadius: ["10%", "10%", "50%", "10%"],
+            }}
+            transition={{
+              duration: 5,
+              ease: "easeInOut",
+              repeat: Infinity,
+              repeatDelay: 1,
+            }}
+          ></motion.div>
+        </motion.div>
+
+        {/* Button hover */}
+        <motion.div
+          variants={gridItemVariants}
+          className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
+        >
+          <motion.button
+            whileTap={{ scale: 0.9 }}
+            whileHover={{
+              scale: 1.1,
+              color: "black",
+              backgroundColor: "#d1d5db",
+            }}
+            transition={{ bounceDamping: 10, bounceStiffness: 600 }}
+            className="bg-emerald-600 w-1/2 py-4 rounded-lg text-2xl text-gray-100 font-semibold tracking-wide"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            Hover me
+          </motion.button>
+        </motion.div>
+
+        {/* Drag & drop */}
+        <motion.div
+          variants={gridItemVariants}
+          className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
+        >
+          <motion.div
+            drag
+            dragConstraints={{ top: -125, right: 125, bottom: 125, left: -125 }}
+            dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }}
+            className="w-1/3 h-1/3 bg-orange-500 rounded-3xl cursor-grabbing"
+          ></motion.div>
+        </motion.div>
+
+        {/* Scroll Progess Indicator */}
+        <motion.div
+          variants={gridItemVariants}
+          className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
+        >
+          <motion.div className="w-40 aspect-square bg-gray-50/20 rounded-xl">
+            <motion.div
+              style={{ scaleY: completionProgress }}
+              className="w-full bg-gray-400 rounded-xl h-full origin-bottom"
             />
-          </a>
-        </div>
-      </div>
+          </motion.div>
+        </motion.div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        {/* Animate SVG */}
+        <motion.div
+          variants={gridItemVariants}
+          className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <motion.svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="w-1/2 stroke-amber-500 stroke-[0.5]"
+          >
+            <motion.path
+              d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+              variants={svgVariants}
+              initial="hidden"
+              animate="visible"
+              transition={{
+                default: {
+                  duration: 2,
+                  ease: "easeInOut",
+                  delay: 1,
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                  repeatDelay: 1,
+                },
+                fill: {
+                  duration: 2,
+                  ease: "easeInOut",
+                  delay: 2,
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                  repeatDelay: 1,
+                },
+              }}
+            />
+          </motion.svg>
+        </motion.div>
+      </motion.section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <section className="flex flex-col gap-10 mb-10" ref={containerRef}>
+        <motion.h1
+          className="text-5xl tracking-wide text-slate-100 text-center"
+          animate={mainControls}
+          initial="invisible"
+          variants={{
+            invisible: { opacity: 0, y: -60 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          transition={{ duration: 1, ease: "easeInOut", delay: 0.3 }}
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          Keep Scrolling
+        </motion.h1>
+        <p className="text-slate-100 font-thin text-4xl w-1/2 mx-auto">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas et
+          praesentium vel quo, asperiores placeat illum quos optio. Vero non
+          saepe consequatur, corporis nemo voluptatem quisquam temporibus
+          veritatis. Velit impedit doloribus, eveniet, vero exercitationem omnis
+          quia tempore eum in ratione ab veritatis. Consequuntur rerum id
+          dignissimos quasi commodi aperiam nisi.
+        </p>
+        <p className="text-slate-100 font-thin text-4xl w-1/2 mx-auto">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+          nihil magnam distinctio eligendi dolorem quam molestias similique
+          adipisci praesentium sed saepe odit sint velit esse voluptatibus
+          impedit, dolor enim ipsam?
+        </p>
+      </section>
     </main>
   );
 }
